0165cfbdf4c8fb5b3697538b35d71f76778190cb
Máté Gujgiczer <mate.gujgiczer@icloud.com>
2020-10-15 18:21:07 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,21 +3,30 @@ import PackageDescription
 
 let package = Package(
     name: "PromiseKit",
-    exclude: [
-        "Sources/AnyPromise.swift",
-        "Sources/Promise+AnyPromise.swift",
-        "Sources/AnyPromise.m",
-        "Sources/dispatch_promise.m",
-        "Sources/GlobalState.m",
-        "Sources/hang.m",
-        "Sources/NSMethodSignatureForBlock.m",
-        "Sources/join.m",
-        "Sources/PMKCallVariadicBlock.m",
-        "Sources/when.m",
-        "Sources/after.m",
-        "Sources/AnyPromise+Private.h",
-        "Sources/AnyPromise.h",
-        "Sources/PromiseKit.h",
-        "Tests"
-    ]
+    platforms: [.iOS(.v10)],
+    products: [
+        .library(name: “PromiseKit", targets: [“PromiseKit"]),
+    ],
+    targets: [
+        .target(
+            name: "Tool",
+            path: "PromiseKit",
+            exclude: [
+                "Sources/AnyPromise.swift",
+                "Sources/Promise+AnyPromise.swift",
+                "Sources/AnyPromise.m",
+                "Sources/dispatch_promise.m",
+                "Sources/GlobalState.m",
+                "Sources/hang.m",
+                "Sources/NSMethodSignatureForBlock.m",
+                "Sources/join.m",
+                "Sources/PMKCallVariadicBlock.m",
+                "Sources/when.m",
+                "Sources/after.m",
+                "Sources/AnyPromise+Private.h",
+                "Sources/AnyPromise.h",
+                "Sources/PromiseKit.h",
+                "Tests"
+            ]
+    )]
 )
